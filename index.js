fd54f4bdb1d0810ba82741aa5b52737ffec41115
YevaUsatova <yeva.usatova@gmail.com>
2021-05-17 21:19:57 -0400

    commit tree e88383c8b3ce519490a8437348886f4fffbdb29c

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let multiply; 
+const num1 = 31;
+const num2 = 2;
+multiply = num1 *num2;
+
+let random;
+random = Math.floor(Math.random() * 10) + 1;
+
+let mod;
+const num3 = 16;
+const num4 = 6;
+mod = num3 % num4;
+
+let max;
+max = Math.max(1,5,10,20);
